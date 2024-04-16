a63ae27ae59fa6b5f83a60a00715ab5b4971bdbf
Erin W <53369160+erinwelch@users.noreply.github.com>
2024-04-16 19:39:21 +1000

    Files Init

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# wp-updater
+# Hyperuix Wordpress Notifier
+
+WordPress plugin that monitors installation for core, plugin and theme updates and emails the client when they are available.
+
+## Installation into a Bedrock site
+
+#### Install
+
+```
+# 1. Get it ready (to use a repo outside of packagist)
+composer config repositories.hyperuix-wp-updater git https://github.com/hyperuix/hyperuix-wp-updater
+
+# 2. Install the Plugin
+composer require hyperuix/hyperuix-wp-updater
+```
+
+#### Activate / Configure
+
+- Activate the plugin
+- Settings > Update Watcher:
+    - For now, please set the cron to `Other Cron` and manually setup a cron for once a month (to start 1 month after the site is launched)
+    - Set the correct emails
+    - Set the recipient's name
+    - Only notify for active themes/plugins
---
+++ email/notification.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+<style>
+    body { font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; font-size: 14px; color: #2f2f2f; }
+    p { margin: 0 0 1em; line-height: 1.5em; }
+</style>
+</head>
+<body>
+
+<p>Hi <?= $name ?>,<br />
+As part of our commitment at HYPERUIX to keep your website secure we have completed a routine review of your website and determined that the following items are out of date and should be updated:</p>
+
+<hr style="margin: 20px 0; border: 0; border-bottom: 1px dotted #eee;" />
+
+<p><?php echo nl2br($message); ?></p>
+
+<hr style="margin: 20px 0; border: 0; border-bottom: 1px dotted #eee;" />
+
+<p>It's important to keep your website up to date to ensure known security vulnerabilities are patched and secured.</p>
+<p>If you would like us to update and patch your website to keep it secure, please contact your Pivotal Agency account manager (or simply reply to this email) and we'll get it organised for you.</p>
+
+<p>Kind regards,<br />
+<a href="https://hyperuix.com.au" style="color: #c9fc01;">HYPERUIX</a></p>
+
+<p><img src="https://stage.hyperuix.com.au/app/uploads/2024/03/logo.png" style="width: 130px; height: auto;" /></p>
+</body>
+</html>
